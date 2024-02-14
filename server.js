2f766c362286fe35e47f8c7fda3eb2cc7166de3b
Eduardo Mejia <mejiaeduardo@yahoo.com>
2024-02-14 11:27:54 -0500

    build schema & model

--- server.js
+++ server.js
@@ -6,10 +6,45 @@ require("dotenv").config();
 
 const PORT = 7000;
 
-// Set up routes
-app.get("/");
+// Setup Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Setup Mongodb connection
+mongoose.connect(process.env.DB_CONNECTION);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection error:"));
+db.once("open", function () {
+  console.log("Connected to MongoDB");
+});
+
+// Define our schema for form data
+
+const formSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  comments: String,
+});
+
+//  Define your model
+
+const sentData = mongoose.model("sentData, formSchema");
+
+// Handle form submission
+app.post("/submit", async function (req, res) {
+  const formData = {
+    name: req.body.name,
+    email: req.body.email,
+    comments: req.body.comments,
+  };
+
+  const dataSent = new sentData(formData);
+  await dataSent.save();
+});
 
 // Star web server
 app.listen(PORT, function () {
-  console.log(`Listening of port ${PORT}`);
+  console.log(`Server listening of port ${PORT}`);
 });
